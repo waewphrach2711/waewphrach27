0f2fbd043374c5f8ff0038450a06132dcc41f7ef
waewphrach2711 <waewphrach2711@gmail.com>
2026-06-18 14:38:21 +0700

    Update program_waewphrach.py

--- program_waewphrach.py
+++ program_waewphrach.py
@@ -1,6 +1,31 @@
-กว้าง = float(input("กรุณาใส่ความกว้าง: "))
-ยาว = float(input("กรุณาใส่ความยาว: "))
+import math
 
-พื้นที่ = กว้าง * ยาว
+while True:
+    shape = input("\nChoose shape (rectangle, triangle, circle, exit): ").lower()
 
-print("พื้นที่คือ", พื้นที่, "ตารางหน่วย")
+    if shape == "exit":
+        break
+
+    try:
+        if shape == "rectangle":
+            width = float(input("Width: "))
+            length = float(input("Length: "))
+            if width > 0 and length > 0:
+                print("Area =", width * length)
+
+        elif shape == "triangle":
+            base = float(input("Base: "))
+            height = float(input("Height: "))
+            if base > 0 and height > 0:
+                print("Area =", 0.5 * base * height)
+
+        elif shape == "circle":
+            radius = float(input("Radius: "))
+            if radius > 0:
+                print("Area =", math.pi * radius**2)
+
+        else:
+            print("Invalid shape")
+
+    except ValueError:
+        print("Please enter numbers only.")
